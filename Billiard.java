9a677de2b4736ba25aad08a799ab3d821ab10213
Sergey Dyakov <s.dyakov9@gmail.com>
2021-04-06 13:56:13 +0300

    first commit

--- Billiard.java
+++ Billiard.java
@@ -1,10 +1,3 @@
-/*
- *  Copyright (C) 2010  Luca Wehrstedt
- *
- *  This file is released under the GPLv2
- *  Read the file 'COPYING' for more information
- */
-
 import javax.swing.JPanel;
 
 import java.awt.Graphics;
--- Speed.java
+++ Speed.java
@@ -1,10 +1,3 @@
-/*
- *  Copyright (C) 2010  Luca Wehrstedt
- *
- *  This file is released under the GPLv2
- *  Read the file 'COPYING' for more information
- */
-
 class Speed {
     private double x = 0;
     private double y = 0;
